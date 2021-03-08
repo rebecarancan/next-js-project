f5d5d55078d6047fc04c4f4f81345c146d4fa287
Rebeca Rancan <rebecarancanrr@gmail.com>
2021-03-08 08:57:09 -0300

    add user component

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/User';
+
 const authIndexPage = () => (
   <div>
     <h1>The auth index page</h1>
+    <User name="Rebeca" age={30} />
   </div>
 );
 
